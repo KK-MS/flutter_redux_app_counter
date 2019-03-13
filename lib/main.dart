34bb0a87bdc678d0c872389836f29c25c688e0d9
KK <kk.contact.mail@gmail.com>
2019-03-13 18:07:56 +0100

    Redux sync code changes!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_redux/flutter_redux.dart'; // Redux Sync calls
+import 'dart:convert';   // to convert Response object to Map object
+
+/*
+ * As per blog
+ * https://medium.com/flutterpub/flutter-redux-thunk-27c2f2b80a3b
+ * Git Source: https://github.com/iamjackslayer/Flutter-Redux-Thunk-on-Medium/blob/master/pubspec.yaml
+ *
+ */
+
+// AppState: Called as 'state', as it contains the current state values and the
+// function or reducers which will predict next state
+class AppState {
+  int _counter;
+
+  int get counter => _counter;
+
+  AppState(this._counter);
+}
+
+// Sync Action
+enum Action {
+  IncrementAction
+}
+
+// Reducer
+AppState reducer(AppState prev, dynamic action) 
+{
+
+  if (action == Action.IncrementAction) {
+
+    debugPrint("\nReducer: Action.IncrementAction");
+
+    AppState newAppState = new AppState(prev.counter + 1);
+
+    return newAppState;
+
+  } 
+
+  // default return previous state
+  return prev;
+
+}
+
+// store that hold our current appstate
+// This can be done from our Main also.
+final store = new Store<AppState>(
+  reducer,
+  initialState: new AppState(0),
+);
+
 
 void main() => runApp(MyApp());
 
@@ -6,21 +58,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    debugPrint("\nMyApp StatelessWidget");
+    return StoreProvider<AppState>(
+      store: store,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+      ), //Material App 
     );
   }
 }
@@ -44,68 +100,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            StoreConnector<AppState, int>(
+              converter: (store) => store.state.counter,
+              builder: (_, counter) {
+                return Text(
+                  '$counter',
+                  style: Theme.of(context).textTheme.display1,
+                );
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      floatingActionButton: StoreConnector<AppState, IncrementCounter>(
+        converter: (store) => () => store.dispatch(Action.IncrementAction),
+        builder: (_, incrementCallback) {
+          return new FloatingActionButton(
+            onPressed: incrementCallback,
+            tooltip: 'Increment',
+            child: new Icon(Icons.add),
+          );
+        },
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+typedef void IncrementCounter(); // This is sync.
